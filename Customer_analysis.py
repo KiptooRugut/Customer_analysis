ff993df89c458266a77b23312af39677da4703c8
KiptooRugut <mccrug97@gmail.com>
2025-05-15 04:03:28 +0300

    Configure matplotlib and seaborn settings for consistent visualizations

--- Customer_analysis.py
+++ Customer_analysis.py
@@ -5,3 +5,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 from sklearn.model_selection import train_test_split
+
+
+def configure_plot_settings():
+    """Configure matplotlib and seaborn settings for consistent visualizations"""
+    sns.set_style("whitegrid")
+    plt.rcParams['figure.figsize'] = (12, 7)
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['axes.titlesize'] = 16
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
